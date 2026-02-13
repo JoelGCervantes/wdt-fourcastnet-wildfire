ea91cfdb7735f4a03e4a58b0e6781b7fe6189752
Joel Garcia-Cervantes <garciac2@login.orca.pdx.edu>
2026-02-13 00:12:07 -0800

    mutliple run training support + requirements.txt

--- FCN/multinode_train.sh
+++ FCN/multinode_train.sh
@@ -3,17 +3,17 @@
 # Purpose: FourCastNet multi-node distributed training
 # Author: garciac2
 
-#SBATCH -p normal
-#SBATCH -t 24:00:00        # Training takes longer than inference
+#SBATCH -p short
+#SBATCH -t 4:00:00        # Training takes longer than inference
 #SBATCH -J FCN_TRAIN
 #SBATCH --output=logs/%x_%j.out
-#SBATCH -N 10               # Number of nodes
+#SBATCH -N 5               # Number of nodes
 #SBATCH --gres=gpu:4       # Use 1 GPU (change to gpu:2 if you want to speed up)
 #SBATCH --ntasks-per-node=1	   # Launch only 1 manager task (torchrun handles multi-process spawning)
 #SBATCH --cpus-per-task=40 # CPU cores (processors) assigned to each individual task (data loading) ***scale with number of gpus***
 #SBATCH --mem=0
 
-module load python/3.12.9
+module load python/3.12.12
 module load cuda/12.9.0
 
 unset LD_LIBRARY_PATH
@@ -31,7 +31,7 @@ export NODE_RANK=$SLURM_PROCID # calculates rank of node
 # Use torchrun to launch training. 
 # --nproc_per_node should match the number of GPUs requested in --gres.
 srun torchrun \
-    --nnodes=10 \
+    --nnodes=5 \
     --nproc_per_node=4 \
     --node_rank=$NODE_RANK \
     --rdzv_id=$SLURM_JOB_ID \
